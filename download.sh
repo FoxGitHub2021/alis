82664896b7169fa81d468ef0521f888bfca24b81
FoxGitHub2021 <90065287+FoxGitHub2021@users.noreply.github.com>
2022-02-04 16:38:01 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,8 +5,8 @@ set -eu
 # and customized Arch Linux system.
 # Copyright (C) 2022 picodotdev
 
-GITHUB_USER="picodotdev"
-BRANCH="master"
+GITHUB_USER="FoxGitHub2021"
+BRANCH="FoxCustom"
 
 while getopts "u:" arg; do
   case ${arg} in
